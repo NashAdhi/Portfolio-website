0e33f271f97dfb47214a3e11cff09685ac2cf1c3
Nash <nageshwarr@gmail.com>
2019-08-05 20:22:23 +0200

    changes 050819

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,49 @@
 
 
 //Smooth Scroll
-$(document).on('click', 'a [href^="#"]', function(event) {
+//$(document).on('click', 'a [href^="#"]', function(event) {
 
 
-    event.preventDefault();
+  //  event.preventDefault();
 
-$('html, head').animate({
-scrollTop: $($.attr(this, 'href')).offset().top    
-}, 5000);
-});
+//$('html, head').animate({
+//scrollTop: $($.attr(this, 'href')).offset().top    
+//}, 5000);
+//});
 
-onclick = "menuToggle()";
+//onclick = "menuToggle()";
 
+//function menuToggle() {
+  //  var X = document.getElementById('navbarSupportedContent');
+
+//if (X.className == 'navbarSupportedContent') {
+
+  //  X.className += ' responsive';
+//}
+//else {
+
+  //  X.className = ' navbarSupportedContent';
+     
+    //}
+//}
+
+//Smooth Scroll
 function menuToggle() {
     var X = document.getElementById('navbarSupportedContent');
 
-if (X.className == 'navbarSupportedContent') {
+    var X = $('#navbarSupportedContent');
+    if (X.hasClass('collapse navbar-collapse')) {
 
-    X.className += ' responsive';
-}
-else {
-
-    X.className = ' navbarSupportedContent';
-     
+        X.removeClass('collapse navbar-collapse');
+    } else {
+        X.removeClass('responsive');
+        X.addClass('collapse navbar-collapse');
     }
 }
+
+
+$(document).ready(() => {
+    $('.navbar-toggler .fas').click(() => {
+        menuToggle();
+    });
+});
